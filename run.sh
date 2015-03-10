5bc46db7a3353f38a75b48b77104fd07ea1b7882
Martin Zajíc <zajca@zajca.cz>
2015-03-10 12:07:56 +0100

    add git

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo 'Server = http://mirror.vpsfree.cz/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-pacman -Syy --noconfirm ffmpeg nodejs imagemagick
+pacman -Syy --noconfirm ffmpeg nodejs git imagemagick
 npm install -g gulp pm2 coffee-script
